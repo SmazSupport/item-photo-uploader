b2da8da982a31abf8607550e81e2611f1796f817
SmazSupport <164916517+SmazSupport@users.noreply.github.com>
2025-03-28 10:49:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
   CLIENT_ID:
     "802096556611-ractpm3v2a5cgsmph03kit4c195ke4j6.apps.googleusercontent.com",
-  API_KEY: "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",
+  API_KEY: "AIzaSyBNvUx3HCuaT_7k2BFtOToOIkumtrKUE2I",
 };
